0061ad13678bfb844922e4120d31c51527d34a0e
Bret Sperry <bret.sperry@gmail.com>
2021-01-02 21:46:31 -0500

    page redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,27 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string //
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page //
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage //
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
@@ -16,9 +34,9 @@ var getRepoIssues = function (repo) {
                     displayWarning(repo);
                 }
             });
-        }
-        else {
-            alert("There was a problem with your request!");
+        } else {
+            // if not successful, redirect to homepage //
+            document.location.replace("./index.html");
         }
     });
 };
@@ -34,6 +52,7 @@ var displayIssues = function (issues) {
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
+
         // create span to hold issue title
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
@@ -57,7 +76,7 @@ var displayIssues = function (issues) {
     }
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     // add text to warning container //
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
     var linkEl = document.createElement("a");
@@ -69,5 +88,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("expressjs/express");
+getRepoName();
 
